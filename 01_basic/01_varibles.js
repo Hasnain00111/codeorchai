a2898b58f98ad8dcddcb51c364c25ac591054cfa
Hasnain00111 <ahmednecrox@gmail.com>
2024-12-14 22:23:54 -0800

    hi their

--- 01_basic/01_varibles.js
+++ 01_basic/01_varibles.js
@@ -1,11 +1,4 @@
-// console.log('this is working')
-
-
-
 let arr = ["ali", "kabeer","israr"]
 console.log(arr)
 
 console.log(arr[1])
-
-
-const accountId = 10000
--- frist.js
+++ frist.js
@@ -1,4 +1,10 @@
-// let array= [1,2,3,4]
-// console.log(array)
-
-console.log('this is working')
+//
+const accountId = 4994;
+let accountEmail = "ahemd@gamil.com";
+var accountPassword = "111222";
+let accountCity = "Islamabad"; // Fixed declaration
+console.log(accountId); // Print accountId to console
+accountEmail = "ahmed@3gmail.com"; // Update email
+accountPassword = "4334"; // Update password
+accountCity = "isla"; // Update city
+console.table([accountId, accountEmail, accountPassword, accountCity]); // Display values
