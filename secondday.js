512cb6f3cb98f263f8a38d1d72ac5e02b45b4f56
Hasnain00111 <ahmednecrox@gmail.com>
2024-12-15 12:43:10 -0800

    you are doing great

---
+++ secondday.js
@@ -0,0 +1,12 @@
+
+
+
+
+console.log("hi hasnain");
+console.log("I am going to learning js");
+
+let name = "hasnain";
+console.log(name);
+
+let age = 16;
+console.log(`Your age is: ${age}`);
---
+++ that.js
@@ -0,0 +1,13 @@
+// 03let.js
+let message = "Hello, this is a test file.";
+console.log(message);
+
+// profile.js
+const profile = {
+    username: "hasnainahmed",
+    isFollow: false,
+    followers: 333,
+    following: 3456
+};
+
+console.log(profile);
