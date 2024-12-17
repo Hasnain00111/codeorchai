720b6c3ece5093bc39d5423fa5751b9aafec16a9
Hasnain00111 <ahmednecrox@gmail.com>
2024-12-16 22:38:56 -0800

    smart

---
+++ forth.js
@@ -0,0 +1,12 @@
+let score = "ahmed"
+console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+let isloggedIn = "ahmed"
+let bolleanIsLoggedIn = Boolean(isLoggedIb)
+let someNumber = 44
+ 
+let stringNumber = steing(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
+
